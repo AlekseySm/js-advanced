9652024fb10b43a9372c3bd57ed7489661caeeda
Oleksii Smetana <aleksey.smetana90@gmail.com>
2021-02-10 22:13:23 +0200

    Class work #1 less 5

--- lesson_5/Classwork/task1.js
+++ lesson_5/Classwork/task1.js
@@ -11,3 +11,35 @@
     Стоять -> Поезд {name} остановился. Скорость {speed}
     Подобрать пассажиров -> Увеличивает кол-во пассажиров на Х
 */
+let x = 10;
+function run(newspeed) {
+    if(newspeed != '') {
+        this.speed = newspeed
+    }
+    console.log('Поезд '+ this.name+ ' везет '+ this.countUser +' со скоростью '+ this.speed)
+}
+
+function stop() {
+    this.speed = 0;
+    console.log('Поезд '+ this.name+ ' остановился. Cкорость '+ this.speed)
+}
+
+function addPasenger(adduser) {
+    this.countUser += adduser
+    console.log('Поезд подобрал пасажиров теперь их '+ this.countUser)
+}
+
+
+const Train = {
+    name: 'Intersity',
+    speed: 300,
+    countUser: 100,
+    run,
+    stop,
+    addPasenger,
+}
+
+Train.run();
+Train.stop();
+Train.addPasenger(x);
+Train.run(320);
