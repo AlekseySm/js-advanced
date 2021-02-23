cd8d665ad497b3bcd98e120570bbabe0494b3757
Oleksii Smetana <aleksey.smetana90@gmail.com>
2021-02-23 15:11:29 +0200

    Home work lesson 8/3

--- lesson_8/classWork/3_async.js
+++ lesson_8/classWork/3_async.js
@@ -20,29 +20,51 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('initial script');
 
 
-    async function companyList () {
-        const getCompanyResponse = await fetch("http://www.json-generator.com/api/json/get/ceRHciXcVu?indent=2")
+    async function companyList() {
+        const getCompanyResponse = await fetch('http://www.json-generator.com/api/json/get/ceRHciXcVu?indent=2');
         const companys = await getCompanyResponse.json();
         console.log('main company', companys);
 
-        let html = `    
-                    <div>
-                        ${companys.map((company) => {
-                            return `
-                                    <div class="company">${company.company}</div>
-                                    <div class="balance">${company.balance}</div>
-                                    <div class="registered">${company.registered}</div>
-                                    <div class="address">${Object.values(company.address)}</div>
-                                `;
-                        }).join('')}
-                    </div>
+        let html = `
+                    ${companys.map((company) => {
+                    return `
+                        <div>
+                            <div class="company">${company.company}</div>
+                            <div class="balance">${company.balance}</div>
+                            <div class="registered">${company.registered}</div>
+                            <div class="address">
+                                <span>${Object.values(company.address)}</span>
+                                <button>Show address</button>
+                            </div>
+                        </div>
+                    `;}).join('')}
                 `;
         return html;
     }
 
     let companyListResult = companyList();
-    companyListResult.then( (data) => {
-        //console.log('Final Person:', data);
-        document.getElementById('app').innerHTML = data;
+    companyListResult.then((data) => {
+        let header = `
+        <div class="header">
+            <div class="company">Company</div>
+            <div class="balance">Balance</div>
+            <div class="registered">Показать дату регистрации</div>
+            <div class="address">Показать адресс</div>
+        </div>
+        `;
+        let resultBlock = document.createElement('div');
+        resultBlock.className = 'result';
+        resultBlock.innerHTML = header;
+        resultBlock.querySelector('.header').insertAdjacentHTML( 'afterEnd',data);
+        document.getElementById('app').append(resultBlock);
+
+        document.querySelectorAll('button').forEach((item)=> {
+            item.addEventListener('click', (e) => {
+                e.preventDefault();
+                //console.dir(item.previousElementSibling);
+                item.previousElementSibling.style.display = 'block';
+                item.style.display = 'none';
+            })
+        })
     });
 });
